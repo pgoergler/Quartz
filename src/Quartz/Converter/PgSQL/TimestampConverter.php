6cd91baa7d664377442749cfbfd045dbc1bd6f31
pgoergler <pgoergler@gmail.com>
2015-08-14 09:26:22 +0400

    Fix PgSQL TimestampConvert & TimestampWithTimeoneConverter

--- src/Quartz/Converter/PgSQL/TimestampConverter.php
+++ src/Quartz/Converter/PgSQL/TimestampConverter.php
@@ -9,10 +9,29 @@ namespace Quartz\Converter\PgSQL;
  */
 class TimestampConverter implements \Quartz\Converter\Converter
 {
+    
+    protected function isNullFromDb($data)
+    {
+        return ($data === 'NULL' || $data === 'null' || $data === null || $data === '');
+    }
+    
+    protected function buildDatetime($data, $timezone = null)
+    {
+        if (is_numeric($data))
+        {
+            return new \DateTime('@' . $data, $timezone);
+        } elseif( is_array($data) && array_key_exists('date', $data) && array_key_exists('timezone', $data))
+        {
+            return new \DateTime($data['date'], new \DateTimeZone($data['timezone']));
+        } else
+        {
+            return new \DateTime($data, $timezone);
+        }
+    }
 
     public function fromDb($data, $type, $typeParameter)
     {
-        if ($data === 'NULL' || $data === 'null' || $data === null || $data === '')
+        if ($this->isNullFromDb($data))
         {
             return null;
         }
@@ -20,7 +39,7 @@ class TimestampConverter implements \Quartz\Converter\Converter
         $tz = ( $typeParameter && is_string($typeParameter)) ? new \DateTimeZone($typeParameter) : null;
         if (!is_null($type))
         {
-            return new \DateTime(preg_replace("#^$type '(.*?)'$#", '$1', $data), $tz);
+            return new \DateTime(preg_replace("#^timestamp (tz|with time zone|without time zone)? '(.*?)'$#", '$2', $data), $tz);
         }
         return new \DateTime($data, $tz);
     }
@@ -35,22 +54,21 @@ class TimestampConverter implements \Quartz\Converter\Converter
         $tz = ( $typeParameter && is_string($typeParameter)) ? new \DateTimeZone($typeParameter) : null;
         if (!$data instanceof \DateTime)
         {
-            if (is_numeric($data))
-            {
-                $data = new \DateTime('@' . $data, $tz);
-            } else
-            {
-                $data = new \DateTime($data, $tz);
-            }
+            $data = $this->buildDatetime($data, $tz);
         }
 
         if( $tz )
         {
-            return sprintf("%s '%s'", 'timestamp', $data->setTimezone($tz)->format('Y-m-d H:i:s.u'));
+            return $this->translateToDb($data->setTimezone($tz));
         }
-        return sprintf("%s '%s'", 'timestamp', $data->format('Y-m-d H:i:s.u'));
+        return $this->translateToDb($data);
     }
 
+    protected function translateToDb($data)
+    {
+        return sprintf("%s '%s'", 'timestamp', $data->format('Y-m-d H:i:s.u'));
+    }
+    
     public function translate($type, $parameter)
     {
         $array = '';
--- src/Quartz/Converter/PgSQL/TimestampWithTimezoneConverter.php
+++ src/Quartz/Converter/PgSQL/TimestampWithTimezoneConverter.php
@@ -7,41 +7,12 @@ namespace Quartz\Converter\PgSQL;
  *
  * @author paul
  */
-class TimestampWithTimezoneConverter implements \Quartz\Converter\Converter
+class TimestampWithTimezoneConverter extends TimestampConverter
 {
-
-    public function fromDb($data, $type, $typeParameter)
+    
+    protected function translateToDb($data)
     {
-        if ($data === 'NULL' || $data === 'null' || $data === null || $data === '')
-        {
-            return null;
-        }
-        if (!is_null($type))
-        {
-            return new \DateTime(preg_replace('#^timestamp (tz|with time zone) \'(.*?)\'$#', '$1', $data));
-        }
-        return new \DateTime($data);
-    }
-
-    public function toDb($data, $type, $typeParameter)
-    {
-        if (is_null($data))
-        {
-            return 'NULL';
-        }
-
-        if (!$data instanceof \DateTime)
-        {
-            if (is_numeric($data))
-            {
-                $data = new \DateTime('@' . $data);
-            } else
-            {
-                $data = new \DateTime($data);
-            }
-        }
-
-        return sprintf("%s '%s'", 'timestamp with time zone', $data->format('Y-m-d H:i:s.uP'));
+        return sprintf("%s '%s'", 'timestamp with time zone', $data->format('Y-m-d H:i:s.u'));
     }
 
     public function translate($type, $parameter)
